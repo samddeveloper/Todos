3d9ea52b8e43569d8a294be8f8aea0d27b9e9a3b
sammie slootman <sammieslootman@sammies-MacBook-Air.local>
2024-06-03 00:00:46 +0200

    app component start

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-	return <div className="App">App</div>;
+	const [todo, setTodo] = useState('');
+	const [todos, setTodos] = useState([1,2,3,4]);
+
+	return (
+		<div className="App">
+      <img className="logo" src="/logo.png" alt="Techover" />
+			<ul>
+				{todos.map((todo, i) => (
+					<li key={i}></li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
