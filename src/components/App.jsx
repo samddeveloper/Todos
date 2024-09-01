c14d9214ffa2d44bf94f6c79b30d4cf4295ff920
sammie slootman <sammie.slootman@gmail.com>
2024-09-01 19:59:54 +0200

    removed the API to get a todoapp that works for you

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,38 +6,24 @@ import List from './List';
 function App() {
 	const [todo, setTodo] = useState('');
 	const [todos, setTodos] = useState([]);
-	const [loading, setLoading] = useState(true);
-
-	useEffect(() => {
-		fetch('https://jsonplaceholder.typicode.com/todos')
-			.then ((res) => res.json())
-			.then((data) => {
-				const uncompletedTodos = data.filter(todo => !todo.completed)
-					.map(todo => todo.title);
-				setTodos(uncompletedTodos);
-				setLoading(false)
-			})
-	}, []);
 
 	const addTodo = () => {
 		if (todo !== '') {
-			console.log(todos)
+			console.log(todos);
 			setTodos([...todos, todo]);
 			setTodo('');
 		}
 	};
 
 	const complete = (text) => {
-		const uncompletedTodos = todos.filter((todo) => todo!== text);
+		const uncompletedTodos = todos.filter((todo) => todo !== text);
 		setTodos(uncompletedTodos);
-	}
-
+	};
 
 	return (
 		<div className="App">
 			<Input setTodo={setTodo} todo={todo} addTodo={addTodo} />
-			<List todos={todos} complete={complete} loading={loading} />
-			
+			<List todos={todos} complete={complete} />
 		</div>
 	);
 }
